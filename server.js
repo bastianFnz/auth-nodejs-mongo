48f2272e88eaf2e45668b2c8401d6807b30f3c9e
sebastian fernandez <sebas.fern@hotmail.com>
2020-12-12 03:46:47 +0000

    Fix connection to db

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose= require('mongoose')
-const jwt = require('jsonwebtoken')
 
 const app = express()
 dotenv.config({path:'./config/config.env'})
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
 mongoose.connect(process.env.DB_CONNECT,
-{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
   },
-    ()=> console.log('Db running')
-)
+   (err) => {
+      if (!err) {
+          console.log('Successfully Established Connection with MongoDB')
+      }
+      else {
+          console.log('Failed to Establish Connection with MongoDB with Error: '+ err)
+      }
+    }
+);
 
 app.use(require('./routes/auth.js'))
 
